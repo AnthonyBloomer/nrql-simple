1ce71af28aeb64c88c8f05d8c2885f0f784b1160
Anthony Bloomer <ant0@protonmail.ch>
2018-12-30 15:16:49 +0000

    Update

--- nrql/app.py
+++ nrql/app.py
@@ -12,7 +12,7 @@ def main():
     nrql = NRQL()
     req = nrql.query(args.stmt)
     formatted_json = json.dumps(req, sort_keys=True, indent=4)
-    colorful_json = highlight(unicode(formatted_json, 'UTF-8'), lexers.JsonLexer(), formatters.TerminalFormatter())
+    colorful_json = highlight(str(formatted_json, 'UTF-8'), lexers.JsonLexer(), formatters.TerminalFormatter())
     print(colorful_json)
 
 
