c100aa98bdabb908e546275f53c4696f1a93a0f9
Anthony Bloomer <ant0@protonmail.ch>
2019-01-04 14:33:06 +0000

    Only return the results array by default. Pass verbose flag to return everything. Fixed Region logic

--- nrql/api.py
+++ nrql/api.py
@@ -9,6 +9,9 @@ class NRQL(object):
         self._account_id = account_id
         self._url = "https://insights-api.newrelic.com/v1/accounts/%s/query?nrql=%s"
         self._eu_url = "https://insights-api.eu.newrelic.com/v1/accounts/%s/query?nrql=%s"
+        self._region = 'US'
+        self._verbose = False
+        self._environment = None
 
     @property
     def api_key(self):
@@ -26,6 +29,30 @@ class NRQL(object):
     def account_id(self, account_id):
         self._account_id = account_id
 
+    @property
+    def region(self):
+        return self._region
+
+    @region.setter
+    def region(self, region):
+        self._region = region
+
+    @property
+    def verbose(self):
+        return self._verbose
+
+    @verbose.setter
+    def verbose(self, verbose):
+        self._verbose = verbose
+
+    @property
+    def environment(self):
+        return self._environment
+
+    @environment.setter
+    def environment(self, environment):
+        self._environment = environment
+
     def query(self, stmt):
         if not self.api_key or not self.account_id:
             if os.environ.get('NR_API_KEY') and os.environ.get('NR_ACCOUNT_ID'):
@@ -33,7 +60,10 @@ class NRQL(object):
                 self._account_id = os.environ.get('NR_ACCOUNT_ID')
             else:
                 raise Exception("An api key and account id is required.")
-        if self.api_key.startswith('eu'):
+        if not self.region == 'US':
             self._url = self._eu_url
         req = requests.get(self._url % (self.account_id, stmt), headers={"X-Query-Key": self.api_key})
-        return req.json()
+        response = req.json()
+        if self.verbose:
+            response = response['results']
+        return response
--- nrql/app.py
+++ nrql/app.py
@@ -8,11 +8,22 @@ from pygments import highlight, lexers, formatters
 def main():
     parser = ArgumentParser(prog='nrql-simple')
     parser.add_argument('stmt', help="The NRQL statement.")
+    parser.add_argument('--verbose', '--v',
+                        dest='verbose',
+                        action='store_true',
+                        help="Pass this flag if you want the whole response. "
+                             "The program only outputs the results array by default.")
+    parser.add_argument('region',
+                        nargs='?',
+                        default='US',
+                        help="Pass this flag to set your region (EU or US) By default the region is set to US.")
     args = parser.parse_args()
     nrql = NRQL()
+    nrql.region = args.region
+    nrql.verbose = args.verbose
     req = nrql.query(args.stmt)
     formatted_json = json.dumps(req, sort_keys=True, indent=4)
-    colorful_json = highlight(str(formatted_json, 'UTF-8'), lexers.JsonLexer(), formatters.TerminalFormatter())
+    colorful_json = highlight(str(formatted_json).encode('utf-8'), lexers.JsonLexer(), formatters.TerminalFormatter())
     print(colorful_json)
 
 
